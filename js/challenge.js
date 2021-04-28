d9a6761baea3ad36fadfce38359996329eb89b6c
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-04-28 13:23:53 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -34,7 +34,11 @@ submitForm.addEventListener("submit", function(e){
   e.preventDefault();
   const commentInput = document.getElementById("comment-input").value;
   console.log(commentInput)
-  comment.innerText += commentInput
+  const ul = document.createElement("ul")
+  const li = document.createElement("li")
+  comment.appendChild(ul)
+  ul.appendChild(li)
+  li.innerText += commentInput
 })
 
 
