d27d2c32769ec74fe7ec0c84abd19fbc015ff9a6
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-04-28 13:13:41 +0000

    Automatically backed up by Learn

---
+++ js/challenge.js
@@ -0,0 +1,30 @@
+const counterValue = document.getElementById("counter")
+let numberValue = parseInt(counterValue.innerHTML)
+console.log(numberValue)
+const plus = document.getElementById("plus")
+const minus = document.getElementById("minus")
+const heart = document.getElementById("heart")
+const likes = document.querySelector(".likes")
+
+setInterval(function(){
+    counterValue.innerText = numberValue += 1;
+}, 1000);
+
+plus.addEventListener("click", function(){
+  console.log("clicked");
+  counterValue.innerText = numberValue += 1;
+})
+
+minus.addEventListener("click", function(){
+  console.log("clicked");
+  counterValue.innerText = numberValue -= 1;
+})
+
+heart.addEventListener("click", function(){
+  console.log("clicked");
+  const li = document.createElement("li")
+  li.innerHTML = `${numberValue} has been liked 1 time`
+  likes.appendChild(li)
+})
+
+
